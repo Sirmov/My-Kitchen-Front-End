8364f0b286cb08cbdaa83f09333134c1388567a5
Nikola Sirmov <sirmov0213@gmail.com>
2025-03-29 19:10:27 +0200

    Created use storage hooks

---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+export enum StorageType {
+    LocalStorage,
+    SessionStorage,
+}
+
+function useStorage<T>(
+    storage: StorageType,
+    key: string,
+    defaultValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+    if (typeof window === 'undefined') {
+        throw new Error('Window is not defined. Are you running this code on the client.');
+    }
+
+    let usedStorage: Storage;
+
+    if (storage === StorageType.LocalStorage) {
+        usedStorage = window.localStorage;
+    } else {
+        usedStorage = window.sessionStorage;
+    }
+
+    const [value, setValue] = useState<T>((): T => {
+        const saved = usedStorage.getItem(key);
+
+        if (saved !== null) {
+            return JSON.parse(saved);
+        } else {
+            usedStorage.setItem(key, JSON.stringify(defaultValue));
+            return defaultValue;
+        }
+    });
+
+    useEffect(() => {
+        usedStorage.setItem(key, JSON.stringify(value));
+    }, [key, value, usedStorage]);
+
+    return [value, setValue];
+}
+
+export default useStorage;
