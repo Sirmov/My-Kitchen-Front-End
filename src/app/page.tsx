6b779f2fa0a275a0f911f59753c41b1ba7e7d10d
Nikola Sirmov <sirmov0213@gmail.com>
2025-03-29 19:16:50 +0200

    Added home page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import womanCooking from '@public/woman-cooking.jpg';
+
+import styles from './page.module.scss';
+
+import Image from 'next/image';
+
+import { Col, Row } from 'antd';
+
 export default function HomePage() {
-    return <h1>Take control of Your Kitchen</h1>;
+    return (
+        <>
+            <Row className={styles.titleSection}>
+                <Col span={14} className={styles.titleText}>
+                    <h1>Take control of Your Kitchen</h1>
+                    <p>Manage your recipes, prepare your shopping list, watch your supplies in your digital kitchen.</p>
+                </Col>
+                <Col span={10}>
+                    <Image className={styles.titleImage} alt="woman-cooking" src={womanCooking} height={400} />
+                </Col>
+            </Row>
+        </>
+    );
 }
