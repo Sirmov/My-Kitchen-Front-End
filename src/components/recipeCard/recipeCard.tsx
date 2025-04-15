d3b05d9cf459e128ea5ca155681370888e6309f4
Nikola Sirmov <sirmov0213@gmail.com>
2025-04-15 21:34:18 +0300

    Added a delete popup

--- src/components/recipeCard/recipeCard.tsx
+++ src/components/recipeCard/recipeCard.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import styles from './recipeCard.module.scss';
 
-import { Card } from 'antd';
+import { ReactNode, useState } from 'react';
+
+import Link from 'next/link';
+
+import { Card, Popconfirm } from 'antd';
 import Meta from 'antd/es/card/Meta';
-import { ReactNode } from 'react';
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { CloseCircleFilled, DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 import { Recipe } from '@/services/recipesService';
 
@@ -13,24 +18,65 @@ export type RecipeCardProps = {
     recipe: Recipe;
 };
 
-const actions: ReactNode[] = [
-    <EditOutlined key="edit" className={styles.editIcon} />,
-    <DeleteOutlined key="delete" className={styles.deleteIcon} />,
-];
-
 export default function RecipeCard({ recipe }: RecipeCardProps) {
+    const [open, setOpen] = useState(false);
+    const [confirmLoading, setConfirmLoading] = useState(false);
+
+    const showPopconfirm = () => {
+        setOpen(true);
+    };
+
+    const handleOk = () => {
+        setConfirmLoading(true);
+
+        setTimeout(() => {
+            setOpen(false);
+            setConfirmLoading(false);
+        }, 2000);
+    };
+
+    const handleCancel = () => {
+        console.log('Clicked cancel button');
+        setOpen(false);
+    };
+
+    const actions: ReactNode[] = [
+        <EditOutlined key="edit" className={styles.editIcon} />,
+        <Popconfirm
+            key="delete"
+            open={open}
+            icon={<CloseCircleFilled className={styles.confirmDeleteIcon} />}
+            title="Confirm Action"
+            description="Are you sure you want to delete this recipe?"
+            okText="Delete"
+            okButtonProps={{ loading: confirmLoading, variant: 'solid', color: 'red' }}
+            onConfirm={handleOk}
+            cancelButtonProps={{ variant: 'outlined', color: 'red' }}
+            onCancel={handleCancel}>
+            <DeleteOutlined className={styles.deleteIcon} onClick={showPopconfirm} />
+        </Popconfirm>,
+    ];
+
     return (
         <Card
             className={styles.recipeCard}
             hoverable
             cover={
-                // <img src={recipe.imageUrl} alt={recipe.title} className={styles.recipeImage} fill />
-                <div className={styles.recipeImageContainer}>
-                    <ImageWithFallback src={recipe.imageUrl} alt={recipe.title} className={styles.recipeImage} fill />
-                </div>
+                <Link href={`/recipes/${recipe.id}`}>
+                    <div className={styles.recipeImageContainer}>
+                        <ImageWithFallback
+                            src={recipe.imageUrl}
+                            alt={recipe.title}
+                            className={styles.recipeImage}
+                            fill
+                        />
+                    </div>
+                </Link>
             }
             actions={actions}>
-            <Meta title={recipe.title} description={recipe.description} className="aaaa" />
+            <Link href={`/recipes/${recipe.id}`}>
+                <Meta title={recipe.title} description={recipe.description} />
+            </Link>
         </Card>
     );
 }
