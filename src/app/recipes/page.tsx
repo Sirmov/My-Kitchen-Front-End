2943174e3efee1716237cd94bcde9da4fdeb474e
Nikola Sirmov <sirmov0213@gmail.com>
2025-04-15 21:33:18 +0300

    Use recipes service

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,58 +1,59 @@
+'use client';
+
 import styles from './page.module.scss';
 
 import notebook from '@public/notebook.svg';
 
-import { Col, Empty, Row } from 'antd';
+import { useEffect, useState } from 'react';
 
-import RecipeCard, { Recipe } from '@/components/recipeCard/recipeCard';
-import RecipeCardSkeleton from '@/components/recipeCard/skeleton/recipeCardSkeleton';
 import Image from 'next/image';
 
-const recipes: Recipe[] = [
-    {
-        id: '1',
-        imageUrl: 'https://matekitchen.com/wp-content/uploads/2019/08/supa-topcheta-010b.jpg',
-        title: 'Супа топчета',
-        description: 'Топла супа с топчета от кайма.',
-    },
-    {
-        id: '2',
-        imageUrl: 'https://matekitchen.com/wp-content/uploads/2019/08/supa-topcheta-010b.jpg',
-        title: 'Супа топчета',
-        description: 'Топла супа с топчета от кайма.Топла супа с топчета от кайма.Топла супа с топчета от кайма.',
-    },
-    {
-        id: '3',
-        imageUrl: 'https://matekitchen.com/wp-content/uploads/2019/08/supa-topcheta-010b.jpg',
-        title: 'Супа топчета',
-        description: 'Топла супа с топчета от кайма.',
-    },
-    {
-        id: '4',
-        imageUrl: 'https://matekitchen.com/wp-content/uploads/2019/08/supa-topcheta-010b.jpg',
-        title: 'Супа топчета',
-        description: 'Топла супа с топчета от кайма.',
-    },
-    {
-        id: '5',
-        imageUrl: 'https://matekitchen.com/wp-content/uploads/2019/08/supa-topcheta-010b.jpg',
-        title: 'Супа топчета',
-        description: 'Топла супа с топчета от кайма.Топла супа с топчета от кайма.Топла супа с топчета от кайма.',
-    },
-];
+import { Col, Empty, Row } from 'antd';
+
+import RecipeCardSkeleton from '@/components/recipeCard/skeleton/recipeCardSkeleton';
+import RecipeCard from '@/components/recipeCard/recipeCard';
+
+import { getAllRecipes, Recipe } from '@/services/recipesService';
 
 export default function Recipes() {
+    const [recipes, setRecipes] = useState<Recipe[]>();
+
+    useEffect(() => {
+        getAllRecipes()
+            .then((recipes) => {
+                setRecipes(recipes);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    });
+
     return (
         <div className={styles.container}>
             <h1 className={styles.containerTitle}>My Recipes</h1>
-            {recipes.length === 0 ? (
+            {recipes === undefined ? (
+                <Row gutter={[0, { xs: 16, sm: 16, md: 24, lg: 32 }]} align={'stretch'} justify={'space-between'}>
+                    {Array.from({ length: 5 }).map((_, index) => (
+                        <Col
+                            key={index}
+                            xs={{ flex: '100%' }}
+                            md={{ flex: '50%' }}
+                            lg={{ flex: '33%' }}
+                            xl={{ flex: '25%' }}
+                            xxl={{ flex: '20%' }}>
+                            <RecipeCardSkeleton />
+                        </Col>
+                    ))}
+                    <Col flex={'auto'}></Col>
+                </Row>
+            ) : recipes.length === 0 ? (
                 <Empty
                     className={styles.emptyRecipes}
                     image={<Image className={styles.emptyImage} src={notebook} alt="" />}
                     description="No recipes"
                 />
             ) : (
-                <Row gutter={[0, { xs: 8, sm: 16, md: 24, lg: 32 }]} align={'stretch'} justify={'space-between'}>
+                <Row gutter={[0, { xs: 16, sm: 16, md: 24, lg: 32 }]} align={'stretch'} justify={'space-between'}>
                     {recipes.map((r) => (
                         <Col
                             key={r.id}
@@ -64,14 +65,6 @@ export default function Recipes() {
                             <RecipeCard recipe={r} />
                         </Col>
                     ))}
-                    <Col
-                        xs={{ flex: '100%' }}
-                        md={{ flex: '50%' }}
-                        lg={{ flex: '33%' }}
-                        xl={{ flex: '25%' }}
-                        xxl={{ flex: '20%' }}>
-                        <RecipeCardSkeleton />
-                    </Col>
                     <Col flex={'auto'}></Col>
                 </Row>
             )}
