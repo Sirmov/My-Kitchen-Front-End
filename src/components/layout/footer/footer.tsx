b870cc037548ca4683a0819de1b4244c0d909e88
Nikola Sirmov <sirmov0213@gmail.com>
2025-04-21 22:10:54 +0300

    Added simple navigation and contacts

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,11 +1,30 @@
 import styles from './footer.module.scss';
 
+import { Flex } from 'antd';
+
 import { Footer as AntDFooter } from 'antd/es/layout/layout';
+import Link from 'next/link';
 
 export default function Footer() {
     return (
         <AntDFooter className={styles.footer}>
-            Copyright ©{new Date().getFullYear()} My Kitchen. All rights reserved.
+            <Flex justify="space-around" align="center">
+                <ul>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                </ul>
+                <p>Copyright ©{new Date().getFullYear()} My Kitchen. All rights reserved.</p>
+                <ul>
+                    <li>
+                        <p>Made with ❤️</p>
+                    </li>
+                    <li>sirmov0213@gmail.com</li>
+                </ul>
+            </Flex>
         </AntDFooter>
     );
 }
