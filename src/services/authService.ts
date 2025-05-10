842f8662fb3f916c78b2fd1ab9c8c28f005a9341
Nikola Sirmov <sirmov0213@gmail.com>
2025-05-10 23:00:30 +0300

    Implemented mock auth service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,3 +1,4 @@
+import { ProblemDetails } from './services.types';
 
 export type User = {
     _id: string;
@@ -11,25 +12,44 @@ export type User = {
 const users: User[] = [
     {
         _id: '1',
-        username: 'Nikola',
-        email: 'sirmov0213@gmail.com',
+        username: 'sirmov',
+        email: 'sirmov@gmail.com',
+        accessToken: '',
+        refreshToken: '',
+        roles: [],
+    },
+];
+
+export async function login(email: string, password: string): Promise<User | ProblemDetails> {
+    const user = users.find((u) => u.email == email);
+
+    return user
+        ? new Promise((resolve) => setTimeout(resolve, 1000, user))
+        : new Promise((_, reject) =>
+            setTimeout(reject, 1000, {
+                status: 400,
+                type: 'https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1',
+                title: 'Bad request',
+                details: `Email or password not correct.`,
+            })
+        );
+}
+
+export async function register(username: string, email: string, password: string): Promise<User | ProblemDetails> {
+    const user = {
+        _id: (++users.length).toString(),
+        username,
+        email,
         accessToken: '',
         refreshToken: '',
         roles: [],
     };
+
+    users.push(user);
+
+    return new Promise((resolve) => setTimeout(resolve, 1000, user));
 }
 
-export async function register(): Promise<Auth> {
-    return {
-        _id: '1',
-        username: 'Nikola',
-        email: 'sirmov0213@gmail.com',
-        accessToken: '',
-        refreshToken: '',
-        roles: [],
-    };
-}
-
-export async function logout(): Promise<boolean> {
-    return true;
+export async function logout(): Promise<boolean | ProblemDetails> {
+    return new Promise((resolve) => setTimeout(resolve, 1000, true));
 }
