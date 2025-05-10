5ff6b0c5f36b40875f395ded8c1749b085e3799b
Nikola Sirmov <sirmov0213@gmail.com>
2025-05-10 23:02:20 +0300

    Moved recipe components to separate folder

--- src/app/recipes/[recipeId]/page.tsx
+++ src/app/recipes/[recipeId]/page.tsx
@@ -5,8 +5,8 @@ import styles from './page.module.scss';
 import { use, useEffect, useState } from 'react';
 
 import { getRecipe, Recipe } from '@/services/recipesService';
-import RecipeDetails from '@/components/recipeDetails/recipeDetails';
-import RecipeDetailsSkelton from '@/components/recipeDetails/skeleton/recipeDetailsSkeleton';
+import RecipeDetails from '@/components/recipe/recipeDetails/recipeDetails';
+import RecipeDetailsSkelton from '@/components/recipe/recipeDetails/skeleton/recipeDetailsSkeleton';
 
 export default function RecipeDetailsPage({ params }: { params: Promise<{ recipeId: string }> }) {
     const { recipeId } = use(params);
@@ -14,13 +14,12 @@ export default function RecipeDetailsPage({ params }: { params: Promise<{ recipe
 
     useEffect(() => {
         getRecipe(recipeId).then((recipe) => {
-            setRecipe(recipe);
+            setRecipe(recipe as Recipe);
         });
     }, [recipeId]);
 
     return (
         <div className={styles.container}>
-            {/* <RecipeDetailsSkelton /> */}
             {recipe === undefined ? <RecipeDetailsSkelton /> : <RecipeDetails recipe={recipe}></RecipeDetails>}
         </div>
     );
