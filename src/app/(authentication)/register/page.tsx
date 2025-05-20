ceaae2d8ab9774c32aaf6c6d1b48930975a32c63
Nikola Sirmov <sirmov0213@gmail.com>
2025-05-20 21:29:57 +0300

    Handle register

--- src/app/(authentication)/register/page.tsx
+++ src/app/(authentication)/register/page.tsx
@@ -1,28 +1,74 @@
 'use client';
 
+import { useState } from 'react';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 import { LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
-import styles from './page.module.scss';
+import { Button, Checkbox, Col, Flex, Form, Input, Row, notification } from 'antd';
+import FormItem from 'antd/es/form/FormItem';
+import Password from 'antd/es/input/Password';
 
 import foodPlatter from '@public/food-platter.jpg';
 
-import { Button, Checkbox, Col, Flex, Form, Input, Row } from 'antd';
-import FormItem from 'antd/es/form/FormItem';
-import Password from 'antd/es/input/Password';
-import Link from 'next/link';
+import { useAuthContext } from '@contexts/authContext';
+
+import { User, register } from '@services/authService';
+
+import styles from './page.module.scss';
+
+interface RegisterFormValues {
+    email: string;
+    username: string;
+    password: string;
+    confirmPassword: string;
+    agreement: boolean;
+}
 
 export default function RegisterPage() {
+    const router = useRouter();
+
+    const [api, contextHolder] = notification.useNotification();
+    const [form] = Form.useForm<RegisterFormValues>();
+
+    const { setAuth } = useAuthContext();
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(values: RegisterFormValues) {
+        setLoading(true);
+
+        try {
+            const user = await register(values.username, values.email, values.password);
+            setAuth(user as User);
+            router.push('/');
+        } catch {
+            api.error({
+                message: `Error`,
+                description: 'Something went wrong. Please try again later.',
+                placement: 'topRight',
+                showProgress: true,
+                pauseOnHover: true,
+            });
+            form.resetFields();
+        }
+
+        setLoading(false);
+    }
+
     return (
         <Row style={{ height: '90vh' }}>
+            {contextHolder}
             <Col
                 className={styles.imageContainer}
                 style={{ backgroundImage: `url(${foodPlatter.src})` }}
                 xs={{ span: 0 }}
                 md={{ span: 12 }}></Col>
             <Col xs={{ span: 24 }} md={{ span: 12 }}>
-                <Flex justify='center' align='center'>
+                <Flex justify="center" align="center">
                     <div className={styles.container}>
                         <h1 className={styles.formTitle}>Register</h1>
-                        <Form className={styles.registerForm} name="registerForm">
+                        <Form className={styles.registerForm} name="registerForm" form={form} onFinish={handleSubmit}>
                             <FormItem
                                 name="email"
                                 rules={[
@@ -59,7 +105,7 @@ export default function RegisterPage() {
                                 <Password prefix={<LockOutlined />} placeholder="Password" />
                             </FormItem>
                             <FormItem
-                                name="confirm-password"
+                                name="confirmPassword"
                                 dependencies={['password']}
                                 hasFeedback
                                 rules={[
@@ -89,16 +135,21 @@ export default function RegisterPage() {
                                         rules={[
                                             {
                                                 validator: (_, value) =>
-                                                    value
-                                                        ? Promise.resolve()
-                                                        : Promise.reject(new Error('Should accept agreement')),
+                                                    value ?
+                                                        Promise.resolve()
+                                                    :   Promise.reject(new Error('Should accept agreement')),
                                             },
                                         ]}>
                                         <Checkbox>
                                             I have read the <a href="">agreement</a>
                                         </Checkbox>
                                     </FormItem>
-                                    <Button id={styles.submitButton} type="primary" htmlType="submit" size="large">
+                                    <Button
+                                        id={styles.submitButton}
+                                        type="primary"
+                                        htmlType="submit"
+                                        size="large"
+                                        loading={loading}>
                                         Register
                                     </Button>
                                 </Flex>
